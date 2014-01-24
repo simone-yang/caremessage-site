4f12141e15306e943a2fb9af70d7bec243020f7b
Bruno Campos <brunocampos@gmail.com>
2014-01-24 14:37:30 -0200

    removed typekit from ie and added ie css

--- content/javascript/application.js
+++ content/javascript/application.js
@@ -2,10 +2,20 @@ $(function () {
   $("a.read-bio").popover();
   $("input, textarea").placeholder();
 
-  // Typekit bugs on lower ies
-  if(!($.browser.msie && Number($.browser.version) < 9)) {
-      try{Typekit.load();}catch(e){}
+  var Browser = {
+    Version: function() {
+      var version = 999; // we assume a sane browser
+      if (navigator.appVersion.indexOf("MSIE") != -1)
+        // bah, IE again, lets downgrade version number
+        version = parseFloat(navigator.appVersion.split("MSIE")[1]);
+      return version;
     }
+  }
+
+  // Typekit bugs on lower ies
+  if(!(Browser.Version() < 9)) {
+    try{Typekit.load();}catch(e){}
+  }
 });
 
 $(function(){
