fea938ada64750c83bc699df584c1d3f853f2490
Daniel Naves de Carvalho <danielnc@gmail.com>
2014-01-24 15:45:09 -0200

    concatenating javascript and defering single file

---
+++ content/javascript/all.js
@@ -0,0 +1,8 @@
+//= require include/jquery.min.js
+//= require include/typekit.js
+//= require include/bootstrap.min.js
+//= require include/wufoo.js
+//= require include/retina_min.js
+//= require include/jquery_placeholder.js
+//= require include/stellar_min.js
+//= require include/application.js
